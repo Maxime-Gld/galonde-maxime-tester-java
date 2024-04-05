ce07200c30fd1a7061cc48aedc585af4887d7bcf
Maxime-Gld <Maxime.Gdev@hotmail.com>
2024-04-05 02:53:05 +0200

    ajout de la methode getLastTicketOut() pour les tests bdd

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -9,5 +9,6 @@ public class DBConstants {
     public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=? where ID=?";
     public static final String GET_TICKET = "SELECT t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE FROM ticket t, parking p WHERE p.parking_number = t.parking_number AND t.VEHICLE_REG_NUMBER = ? AND t.OUT_TIME IS NULL ORDER BY t.IN_TIME LIMIT 1";
     // compte le nombre de ticket pour un vehicule selon son immatriculation
-    public static final String GET_NB_TICKET = "select count(ID) from ticket where VEHICLE_REG_NUMBER = ?";
+    public static final String GET_NB_TICKET = "select count(ID) from ticket where VEHICLE_REG_NUMBER = ? AND OUT_TIME IS NOT NULL";
+    public static final String GET_LAST_TICKET_OUT = "SELECT t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE FROM ticket t, parking p WHERE p.parking_number = t.parking_number AND t.VEHICLE_REG_NUMBER = ? AND t.OUT_TIME IS NOT NULL ORDER BY t.OUT_TIME DESC LIMIT 1";
 }
